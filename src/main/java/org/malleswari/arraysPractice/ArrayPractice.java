064505bc3aae382ff352a4b9e980e4b99a933640
ONTIPULI-MALLIS\prasa <ontipuliMallishwari>
2025-08-13 18:32:17 +0530

    updating ArrayPractice

--- src/main/java/org/malleswari/arraysPractice/ArrayPractice.java
+++ src/main/java/org/malleswari/arraysPractice/ArrayPractice.java
@@ -4,16 +4,16 @@ public class ArrayPractice {
 
 
     public static void main(String[] args) {
-        int[] integerArray={1,1,4,3,4};
+        int[] integerArray = {1, 1, 4, 3, 4};
 
-        System.out.println("sum of array is ::"+sumOfArrayElements(integerArray));
+        System.out.println("sum of array is ::" + sumOfArrayElements(integerArray));
 
     }
 
-    public static int sumOfArrayElements(int[] array){
-        int sum =0;
-        for (int i=0;i<array.length;i++){
-            sum=sum+array[i];
+    public static int sumOfArrayElements(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
             System.out.println(sum);
         }
         return sum;
